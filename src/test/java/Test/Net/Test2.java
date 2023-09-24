bb29ec753029526e7e43fb3500dfa4989a4cb02f
naziafahad39 <naziafahad39@gmail.com>
2023-09-24 16:11:02 +0530

    Master Firdous

--- src/test/java/Test/Net/Test2.java
+++ src/test/java/Test/Net/Test2.java
@@ -6,6 +6,10 @@ public class Test2 {
 	{
 		System.out.println("Test Master");
 		System.out.println("Master1");
+		System.out.println("Master2");
+		System.out.println("Master3");
+		System.out.println("Master4");
+		
 
 	}
 
