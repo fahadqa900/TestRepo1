6d3d315d58b2d2adacad72897757fe69e6b59184
naziafahad39 <naziafahad39@gmail.com>
2023-09-24 15:37:04 +0530

    After rebase committing feature branch changes into master

--- src/test/java/Test/Net/Test2.java
+++ src/test/java/Test/Net/Test2.java
@@ -4,11 +4,17 @@ public class Test2 {
 
 	public static void main(String[] args) 
 	{
+
 		System.out.println("Test Master");
 		System.out.println("Master1");
 		System.out.println("Master2");
 		System.out.println("Master3");
 		System.out.println("Master4");
+		
+
+
+		System.out.println("Test1");
+		System.out.println("Test2");
 		
 
 	}
